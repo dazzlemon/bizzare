b0eef65ca874f6cb89a2be124db8be424eb370ad
DefinitelyLD <drobnyyalex@gmail.com>
2020-07-06 14:43:39 +0300

    tazasho

--- production/bizzare_proj/player/player.gd
+++ production/bizzare_proj/player/player.gd
@@ -5,11 +5,20 @@ const MAX_SPEED = 300
 const DECEL = 2000
 
 var velocity
+ 
+enum{
+	MOVE,
+	ATTACK
+}
+
+var state = MOVE
 
 func _ready():
 	velocity= Vector2.ZERO
 	
 func _physics_process(delta):
+
+	
 	var input_vector = Vector2.ZERO
 	input_vector.x = Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")
 	input_vector.y = Input.get_action_strength("ui_down") - Input.get_action_strength("ui_up")
@@ -21,3 +30,6 @@ func _physics_process(delta):
 		velocity = velocity.move_toward(Vector2.ZERO, DECEL * delta)
 	
 	velocity = move_and_slide(velocity)
+	if Input.is_action_just_pressed("attack"):
+		print("-5hp")
+		get_node("CollisionShape2D").disabled= true
