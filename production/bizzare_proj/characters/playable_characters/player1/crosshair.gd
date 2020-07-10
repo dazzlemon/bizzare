8f0bdafc5965a832112050bdb8f8de27260ad78c
dazzlemon <daanill13470@gmail.com>
2020-07-10 09:05:57 +0300

    fixed invisible attack hitbox for enemy1, fixed old attack not following crosshair, still need to remake it

--- production/bizzare_proj/characters/playable_characters/player1/crosshair.gd
+++ production/bizzare_proj/characters/playable_characters/player1/crosshair.gd
@@ -7,6 +7,5 @@ func _ready():
 func _process(_delta):
 	position = get_node("sprite").get_viewport().get_mouse_position()
 	
-	var body_cross_vec = get_node("../").global_position - global_position 
-	var rad = body_cross_vec.angle()
-	get_node("../MeleeAttack").rotation = rad#it doesn't work idk why
+	var body_cross_dir = get_node("../MeleeAttack").get_angle_to(get_global_mouse_position())
+	get_node("../MeleeAttack").rotation += body_cross_dir
