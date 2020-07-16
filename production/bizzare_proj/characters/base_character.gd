f92656c0654f0f12f9f72559ef013a3d17426ab6
dazzlemon <daanill13470@gmail.com>
2020-07-16 15:46:04 +0300

    factored out fully working take_damage(damage) and die() methods

--- production/bizzare_proj/characters/base_character.gd
+++ production/bizzare_proj/characters/base_character.gd
@@ -38,20 +38,6 @@ func take_damage(damage):
 			stats.set_armor(0)
 	else:
 		stats.health -= damage
-		
-var loot_scene = preload("res://loot/Coin.tscn")
-
-func drop_loot():
-	var random = randi() % 100 + 1
-	print(random)
-	if random <= 50:
-		print("<=50")
-		var loot = loot_scene.instance()
-		loot.global_position = global_position
-		get_node("../").add_child(loot)
-	else:
-		print(">50")	
 	
 func die():
 	queue_free()
-	drop_loot()
--- production/bizzare_proj/characters/npcs/npc.gd
+++ production/bizzare_proj/characters/npcs/npc.gd
@@ -1,18 +1,1 @@
 extends "res://characters/base_character.gd"
-
-#var loot_scene = preload("res://loot/Coin.tscn")
-
-func drop_loot():
-	var random = randi() % 100 + 1
-	print(random)
-	if random <= 50:
-		print("<=50")
-		var loot = loot_scene.instance()
-		loot.global_position = global_position
-		get_node("../").add_child(loot)
-	else:
-		print(">50")
-
-func die():
-	.die()
-	#drop_loot()
