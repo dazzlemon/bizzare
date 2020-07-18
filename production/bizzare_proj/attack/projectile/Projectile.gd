d2189a856533a37b9ebc69c94c0997fc6013b863
dazzlemon <daanill13470@gmail.com>
2020-07-18 17:00:35 +0300

    even more almost finished projectile attack

--- production/bizzare_proj/attack/projectile/Projectile.gd
+++ production/bizzare_proj/attack/projectile/Projectile.gd
@@ -1,15 +1,17 @@
 extends RigidBody2D
 
-var speed = 800
-var LIFETIME = 2
+var speed = 350
+var LIFETIME = 5
 var age = 0
+var direction
+export var damage = 1
 
-func _physics_process(delta):
-	position += transform.x * speed * delta
+func _process(delta):
+	global_position += direction * speed * delta
 	age += delta
 	if age >= LIFETIME:
 		queue_free()
 
 func _on_Projectile_body_entered(body):
-	#body.take_damage()
+	#body.take_damage(damage)
 	queue_free()
--- production/bizzare_proj/attack/projectile/Projectile_Attack.gd
+++ production/bizzare_proj/attack/projectile/Projectile_Attack.gd
@@ -2,11 +2,9 @@ extends Control
 
 var projectile = preload("res://attack/projectile/Projectile.tscn")
 
-const ATTACK_DURATION_SEC = 0.25
 const ATTACK_INTERVAL = 0.15
 
 var attack_active
-var attack_active_timeleft_sec
 var attack_wait_for_next_sec
 
 func _ready():
@@ -18,17 +16,14 @@ func try_attack():
 	get_node("../").owner.add_child(projectile_instance)
 	projectile_instance.transform = get_node("../").global_transform
 	projectile_instance.rotation += get_node("../").get_angle_to(get_global_mouse_position())
+	projectile_instance.direction = (get_global_mouse_position() - get_node("../").global_position).normalized()#use local crosshair's position instead of mice's one
 
 func _process(delta):
 	if attack_wait_for_next_sec > 0:
 		attack_wait_for_next_sec -= delta
 	if get_node("../control").attack_pressed() and attack_wait_for_next_sec <= 0:
 		attack_active = true
-		attack_active_timeleft_sec = ATTACK_DURATION_SEC
-		attack_wait_for_next_sec = ATTACK_INTERVAL + ATTACK_DURATION_SEC
+		attack_wait_for_next_sec = ATTACK_INTERVAL
 	elif attack_active:
-		if attack_active_timeleft_sec > 0:
-			attack_active_timeleft_sec -= delta
-		if attack_active_timeleft_sec <= 0:
-			attack_active = false
+		attack_active = false
 		try_attack()
