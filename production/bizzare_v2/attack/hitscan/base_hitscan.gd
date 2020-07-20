71854987a9de13c8580a7488a40aa7d080122970
dazzlemon <daanill13470@gmail.com>
2020-07-20 18:05:22 +0300

    refactored attack using try_use()

--- production/bizzare_v2/attack/hitscan/base_hitscan.gd
+++ production/bizzare_v2/attack/hitscan/base_hitscan.gd
@@ -17,13 +17,20 @@ func try_attack():
 func _process(delta):
 	if attack_wait_for_next_sec > 0:
 		attack_wait_for_next_sec -= delta
-	if get_node("../control").attack_pressed() and attack_wait_for_next_sec <= 0:
-		attack_active = true
-		attack_active_timeleft_sec = ATTACK_DURATION_SEC
-		attack_wait_for_next_sec = ATTACK_INTERVAL + ATTACK_DURATION_SEC
-	elif attack_active:
+#	if get_node("../control").attack_pressed() and attack_wait_for_next_sec <= 0:
+#		attack_active = true
+#		attack_active_timeleft_sec = ATTACK_DURATION_SEC
+#		attack_wait_for_next_sec = ATTACK_INTERVAL + ATTACK_DURATION_SEC
+	if attack_active:
 		if attack_active_timeleft_sec > 0:
 			attack_active_timeleft_sec -= delta
 		if attack_active_timeleft_sec <= 0:
 			attack_active = false
-		try_attack()
+		#try_attack()
+		
+func try_use():
+	if attack_wait_for_next_sec <= 0:
+		attack_active = true
+		attack_active_timeleft_sec = ATTACK_DURATION_SEC
+		attack_wait_for_next_sec = ATTACK_INTERVAL + ATTACK_DURATION_SEC
+	try_attack()
--- production/bizzare_v2/characters/base_player/control.gd
+++ production/bizzare_v2/characters/base_player/control.gd
@@ -2,18 +2,18 @@ extends "res://characters/control.gd"
 
 onready var spell_1 = get_node("../spell_1")
 onready var spell_2 = get_node("../spell_2")
+onready var attack = get_node("../attack")
 
 func get_input_vector():
 	var input_vector = Vector2.ZERO
 	input_vector.x = Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")
 	input_vector.y = Input.get_action_strength("ui_down") - Input.get_action_strength("ui_up")
 	return input_vector.normalized()
-
-func attack_pressed():
-	return Input.is_action_pressed("attack")
 	
 func _process(delta):
 	if Input.is_action_just_pressed("spell_1"):
 		spell_1.try_use()
 	if Input.is_action_just_pressed("spell_2"):
 		spell_2.try_use()
+	if Input.is_action_pressed("attack"):
+		attack.try_use()
