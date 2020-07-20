126ae8ce8097525cbc3937ca3446e5750f5bfe36
DefinitelyLD <drobnyyalex@gmail.com>
2020-07-20 18:53:19 +0300

    knight spell_1 returned (but not work yet)

--- production/bizzare_v2/characters/base_player/knight/control.gd
+++ production/bizzare_v2/characters/base_player/knight/control.gd
@@ -1,1 +1,7 @@
 extends "res://characters/base_player/control.gd"
+
+func _process(delta):
+	._process(delta)
+	var spell_1 = get_node("../spell_1")
+	var body_cross_dir_q_spell = spell_1.get_angle_to(get_global_mouse_position())
+	spell_1.rotation += body_cross_dir_q_spell
---
+++ production/bizzare_v2/characters/base_player/knight/spell/knigt_spell_1_hitbox.gd
@@ -0,0 +1,4 @@
+extends "res://attack/hitscan/base_hitscan_hitbox.gd"
+
+func _ready():
+	damage = 2.5 * damage
