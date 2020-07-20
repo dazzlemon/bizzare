d963279864774d5ca8494f6f32b7817587e33054
dazzlemon <daanill13470@gmail.com>
2020-07-20 15:25:39 +0300

    added spell interface

--- production/bizzare_v2/characters/base_player/control.gd
+++ production/bizzare_v2/characters/base_player/control.gd
@@ -1,5 +1,8 @@
 extends "res://characters/control.gd"
 
+onready var spell_1 = get_node("../spell_1")
+onready var spell_2 = get_node("../spell_2")
+
 func get_input_vector():
 	var input_vector = Vector2.ZERO
 	input_vector.x = Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")
@@ -8,3 +11,9 @@ func get_input_vector():
 
 func attack_pressed():
 	return Input.is_action_pressed("attack")
+	
+func _process(delta):
+	if Input.is_action_just_pressed("spell_1"):
+		spell_1.try_use()
+	if Input.is_action_just_pressed("spell_2"):
+		spell_2.try_use()
