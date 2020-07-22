bb7fc09afde3c08dbf9748ef42c0eb2649dbb44e
dazzlemon <daanill13470@gmail.com>
2020-07-22 06:20:49 +0300

    refactored AI for melee enemies(now it would be easier to inherit from it + less error prone and overall cleaner)

--- production/bizzare_v2/src/characters/base_npc/goblin/goblin_control.gd
+++ production/bizzare_v2/src/characters/base_npc/goblin/goblin_control.gd
@@ -2,9 +2,12 @@
 class_name GoblinControl
 extends BaseNPCControl
 
+export var wander_target_range = 4
+
+onready var this_enemy = get_node("../")
 onready var playerDetectionZone = $player_detection_zone
 onready var wanderController = $wander_controller
-export var wander_target_range = 4
+
 enum{
 	IDLE,
 	WANDER,
@@ -16,73 +19,61 @@ var state = CHASE
 func _ready():
 	pick_random_state([IDLE, WANDER])
 
+
 func get_input_vector():
-	var input_vector = Vector2.ZERO
-	var this_enemy = get_node("../")
+	var input_vector: Vector2 
 	match state:
 		IDLE:
-			idle()
+			input_vector = idle()
 		WANDER:
-			#wander(this_enemy, input_vector)
-			seek_player()
-			if wanderController.get_time_left() == 0:
-				state = pick_random_state([IDLE, WANDER])
-				wanderController.start_wander_timer(rand_range(1, 1.5))
-				
-			var direction = this_enemy.global_position.direction_to(wanderController.position_target)
-			input_vector = direction
-			
-			if this_enemy.global_position.distance_to(wanderController.position_target) <= wander_target_range :
-				state = pick_random_state([IDLE, WANDER])
-				wanderController.start_wander_timer(rand_range(1, 3))
+			input_vector = wander()
 		CHASE:
-			#chase(this_enemy, input_vector)
-			var player = playerDetectionZone.player
-			if player != null:
-				var direction = this_enemy.global_position.direction_to(player.global_position)
-				input_vector = direction
-			else:
-				state = IDLE
-			#sprite.flip_h = velocity.x<0 (rotate spite in player direction)
+			input_vector = chase()
 	return input_vector.normalized()
+
 
 func attack_pressed():
 	var rng = RandomNumberGenerator.new()
 	rng.randomize()
 	return rng.randi_range(0, 1) == 0
 
+
 func seek_player():
 	if playerDetectionZone.can_see_player():
 		state = CHASE
 
+
 func pick_random_state(state_list):
 	state_list.shuffle()
 	return state_list.pop_front()
-	
-func idle():
+
+
+func idle() -> Vector2:
 	seek_player()
 	if wanderController.get_time_left() == 0:
 		state = pick_random_state([IDLE, WANDER])
 		wanderController.start_wander_timer(rand_range(1, 1.5))
-		
-func wander(this_enemy, input_vector):
+	return Vector2.ZERO
+
+
+func wander() -> Vector2:
 	seek_player()
 	if wanderController.get_time_left() == 0:
 		state = pick_random_state([IDLE, WANDER])
 		wanderController.start_wander_timer(rand_range(1, 1.5))
-				
-	var direction = this_enemy.global_position.direction_to(wanderController.target_position)
-	input_vector = direction
-			
-	if this_enemy.global_position.distance_to(wanderController.target_position) <= wander_target_range :
+	var direction = this_enemy.global_position.direction_to(wanderController.position_target)
+	if this_enemy.global_position.distance_to(wanderController.position_target) <= wander_target_range :
 		state = pick_random_state([IDLE, WANDER])
 		wanderController.start_wander_timer(rand_range(1, 3))
-		
-func chase(this_enemy, input_vector):
+	return direction
+
+
+func chase() -> Vector2:
 	var player = playerDetectionZone.player
+	var direction := Vector2.ZERO
 	if player != null:
-		var direction = this_enemy.global_position.direction_to(player.global_position)
-		input_vector = direction
+		direction = this_enemy.global_position.direction_to(player.global_position)
 	else:
 		state = IDLE
-	#sprite.flip_h = velocity.x<0 (rotate spite in player direction)
+#	sprite.flip_h = velocity.x < 0 #(rotate spite in player direction)
+	return direction
