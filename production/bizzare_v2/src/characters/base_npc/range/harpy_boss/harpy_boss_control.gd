e64368f34ba2d047581ea786e6f5d7cf5c7b8a25
DefinitelyLD <drobnyyalex@gmail.com>
2020-08-06 10:18:56 +0300

    boss phase 1

--- production/bizzare_v2/src/characters/base_npc/range/harpy_boss/harpy_boss_control.gd
+++ production/bizzare_v2/src/characters/base_npc/range/harpy_boss/harpy_boss_control.gd
@@ -1,7 +1,11 @@
 class_name HarpyBossControl
 extends BaseNPCRangeControl
 
-var projectile = preload("res://src/characters/base_npc/range/harpy_boss/attack/harpy_boss_projectile.tscn")
+onready var projectile = preload("res://src/characters/base_npc/range/harpy_boss/attack/harpy_boss_projectile.tscn")
+onready var stats = get_node("../stats")
+onready var get_root = get_node("../")
+
+var phase = 0
 
 func wander() -> Vector2:
 	state = States.IDLE
@@ -9,15 +13,20 @@ func wander() -> Vector2:
 
 
 func _on_stats_phase1():
-	phase1()
+	phase = 1 
+	get_node("phase_wave_cd").start(0.001)
 
 
-func phase1():
-	for i in range(0, 18, 1):
-		print(i)
+func projectile_circle(amount, start):
+	for i in range(0, amount, 1):
 		var projectile_instance = projectile.instance()
-		get_node("../").owner.add_child(projectile_instance)
-		projectile_instance.transform = get_node("../").global_transform
+		get_root.owner.call_deferred("add_child", projectile_instance)
+		projectile_instance.transform =  get_root.global_transform
 		#projectile_instance.rotation += get_node("../").get_angle_to(get_node("../crosshair").global_position)#???useless?
-		projectile_instance.direction = (Vector2(sin(deg2rad(20 * i)), cos(deg2rad(20 * i))) )#- get_node("../").global_position).normalized() 
-		projectile_instance.damage = get_node("../stats").dmg
+		projectile_instance.direction = (Vector2(sin(deg2rad(start + 360/amount * i)), cos(deg2rad(start + 360/amount * i))) )#- get_node("../").global_position).normalized() 
+		projectile_instance.damage = stats.dmg
+
+
+func _on_phase_wave_cd_timeout():
+	if phase == 1:
+		projectile_circle(18,0)
