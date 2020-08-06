3cd74d8d34da080afc121be4e21f160d7dfd4b85
dazzlemon <daanill13470@gmail.com>
2020-08-06 11:01:21 +0300

    harpy boss update_phase

--- production/bizzare_v2/src/characters/base_npc/boss_stats.gd
+++ production/bizzare_v2/src/characters/base_npc/boss_stats.gd
@@ -1,10 +1,13 @@
 class_name BossStats
 extends BaseNPCStats
 
-signal phase1
+signal phase1#mb rename these to represent that they switch state*****
 signal phase2
 signal phase3
 
+const phase_map = {1: 0.75, 2: 0.5, 3: 0.25}# % of health where phase x starts
+
+onready var control = get_node("../control")
 
 func set_health(value) -> void:
 	health = value
@@ -12,10 +15,13 @@ func set_health(value) -> void:
 		emit_signal("no_health")
 	if not player_detection_zone.has_player_inside_fov() :
 		emit_signal("damage_from_behind")
-	#REWORK TO MAP
-	if health > max_health * 0.5  and health <= max_health * 0.75:
-		emit_signal("phase1")
-	if health > max_health * 0.25  and health <= max_health * 0.5:
-		emit_signal("phase2")
-	if health > 0  and health <= max_health * 0.25:
-		emit_signal("phase3")
+	update_phase()
+
+func update_phase():
+	for phase in range(3, 0, -1):
+		if phase_check(phase):
+			emit_signal("phase" + str(phase))
+			return
+
+func phase_check(phase) -> bool:
+	return control.phase < phase and health <= max_health * phase_map.get(phase)
