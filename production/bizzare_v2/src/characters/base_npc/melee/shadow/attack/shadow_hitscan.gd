536b313256bcf1af781c984f03341a4f155f2b4a
DefinitelyLD <drobnyyalex@gmail.com>
2020-08-17 14:01:45 +0300

    shadow + first hit crit

---
+++ production/bizzare_v2/src/characters/base_npc/melee/shadow/attack/shadow_hitscan.gd
@@ -0,0 +1,6 @@
+class_name ShadowHitscan
+extends BaseHitscanEnemy
+
+func _ready():
+	ATTACK_DURATION_SEC = 0.25
+	ATTACK_INTERVAL = 0.1
--- production/bizzare_v2/src/characters/base_npc/melee/shadow/shadow_control.gd
+++ production/bizzare_v2/src/characters/base_npc/melee/shadow/shadow_control.gd
@@ -1,2 +1,32 @@
 class_name ShadowControl
 extends BaseNPCMeleeControl
+
+var critical := true
+onready var default_damage = 1
+
+
+func _ready():
+	randomize()
+
+
+func _process(delta):
+	._process(delta)
+	var player = playerDetectionZone.player
+	if player != null and get_parent().global_position.distance_to(playerDetectionZone.player.global_position) < 25:
+		melee_attack()
+
+func melee_attack():
+	var player = playerDetectionZone.player
+	if player != null and get_node("../pivot/sprite").visible:
+		get_node("../crosshair").global_position = player.global_position
+		if critical: 
+			var rng = RandomNumberGenerator.new()
+			rng.randomize()
+			get_node("../attack/hitbox").damage  = default_damage * rng.randi_range(10, 20)
+			critical = false
+		else: 
+			get_node("../attack/hitbox").damage  = default_damage 
+		print(get_node("../attack/hitbox").damage )
+		get_node("../attack").try_use()
+		
+		
