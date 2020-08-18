0d09d9ea5766ddcc999921f5f2ef55560697d696
dazzlemon <daanill13470@gmail.com>
2020-08-18 17:29:49 +0300

    fixed knight's aoe back

--- production/bizzare_v2/src/characters/base_player/knight/spells/knight_aoe_hitbox.gd
+++ production/bizzare_v2/src/characters/base_player/knight/spells/knight_aoe_hitbox.gd
@@ -5,7 +5,7 @@ extends BaseHitscanHitbox
 var KNOCKBACK_MULTIPLIER := 225# 225 optimalnoe znachenie #500 dlya testa
 
 func _ready() -> void:
-	damage = get_node("../../stats").dmg * 2.5 #* damage#zvuchit kak govno?(pochemu ne prosto 2.5(2.5 * 1 = 2.5))
+	damage = get_node("../../../stats").dmg * 2.5 #* damage#zvuchit kak govno?(pochemu ne prosto 2.5(2.5 * 1 = 2.5))
 	
 
 func knockback(body) -> Vector2:# WIP
