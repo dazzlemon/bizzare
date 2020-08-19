a3b1abb8037dc22d215c66b779ad3c814da9c6c1
DefinitelyLD <drobnyyalex@gmail.com>
2020-08-19 13:53:52 +0300

    spell_cd_duration_wave

---
+++ production/bizzare_v2/src/spell_cd/spell_cd_duration_wave/spell_cd_duration_wave.gd
@@ -0,0 +1,23 @@
+# any action that has to have cooldown + duration + wave interval
+#
+# how to use:
+#   1) change INTERVAL, DURATION , WAVE_INTERVAL in _ready() function(next time use will be available after (INTERVAL+DURATION)sec passed)
+#   2) override _start(), _stop() functions with respective <action> code
+#   3) call try_use, when want to use action(it will use if cooldown is 0)
+class_name SpellCdDurationWave
+extends SpellCdDuration
+
+onready var _wave_interval = $wave_interval
+var _WAVE_INTERVAL:float
+
+func _on_wave_interval_timeout():
+	_start()
+
+
+func _on_duration_timeout():
+	_stop()
+
+
+func _timer_start():
+	_wave_interval.start(_WAVE_INTERVAL)
+	._timers_start()
