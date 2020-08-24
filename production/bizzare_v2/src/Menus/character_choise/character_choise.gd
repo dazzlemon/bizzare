e60b42192b9a13faac4c98ce0b5c3b53664524d3
DefinitelyLD <drobnyyalex@gmail.com>
2020-08-24 15:29:22 +0300

    Character choice screen (WIP)

---
+++ production/bizzare_v2/src/Menus/character_choise/character_choise.gd
@@ -0,0 +1,31 @@
+extends Panel
+
+onready var profile_image= get_node("MarginContainer/HBoxContainer/CenterContainer/TextureRect")
+onready var class_type = get_node("MarginContainer/HBoxContainer/VBoxContainer/Class/Data")
+onready var health = get_node("MarginContainer/HBoxContainer/VBoxContainer/Health/Data")
+onready var armor = get_node("MarginContainer/HBoxContainer/VBoxContainer/Armor/Data")
+onready var speed = get_node("MarginContainer/HBoxContainer/VBoxContainer/Speed/Data")
+onready var damage = get_node("MarginContainer/HBoxContainer/VBoxContainer/Damage/Data")
+
+func _ready():
+	var class_data = load("res://src/Menus/character_choise/knight_profile.tres")
+	update_class_display(class_data)
+
+
+func update_class_display(class_data):
+	profile_image.texture = class_data.image
+	class_type.text = class_data.class_type
+	health.text = str(class_data.health)
+	armor.text = str(class_data.armor)
+	speed.text = str(class_data.speed)
+	damage.text = str(class_data.damage)
+
+
+func _on_KnightButton_pressed():
+	var class_data = load("res://src/Menus/character_choise/knight_profile.tres")
+	update_class_display(class_data)
+
+
+func _on_ArcherButton_pressed():
+	var class_data = load("res://src/Menus/character_choise/archer_profile.tres")
+	update_class_display(class_data)
---
+++ production/bizzare_v2/src/Menus/character_choise/choise_screen_stats.gd
@@ -0,0 +1,9 @@
+extends Resource
+class_name ChoiseScreenStats
+
+export (String) var class_type
+export(float) var health = 100
+export(float) var armor = 100
+export(float) var speed = 100
+export(float) var damage = 100
+export(Image) var image = null
