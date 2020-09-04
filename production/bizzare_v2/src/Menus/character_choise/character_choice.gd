6718fe5c75fa6fd3b394c381891c69d6b60aafd6
dazzlemon <daanill13470@gmail.com>
2020-09-04 20:55:04 +0300

    refactor

--- production/bizzare_v2/src/Menus/character_choise/character_choice.gd
+++ production/bizzare_v2/src/Menus/character_choise/character_choice.gd
@@ -1,32 +1,34 @@
 extends Panel
 
-var player_class = player_classes.KNIGHT
+class PlayableChar:
+	var scene
+	var stats_script
 
+	func _init(scene, stats_script):
+		self.scene = scene
+		self.stats_script = stats_script
+
+var knight = PlayableChar.new(
+	preload("res://src/characters/base_player/knight/knight.tscn"),
+	preload("res://src/characters/base_player/knight/knight_stats.gd")
+)
+var archer = PlayableChar.new(
+	preload("res://src/characters/base_player/archer/archer.tscn"),
+	preload("res://src/characters/base_player/archer/archer_stats.gd")
+)
+
+onready var player_class = knight
 onready var icon = get_node("VBoxContainer/TextureRect")
 
-const knight = preload("res://src/characters/base_player/knight/knight.tscn")
-const archer = preload("res://src/characters/base_player/archer/archer.tscn")
-
-
-enum player_classes {
-	KNIGHT,
-	ARCHER,
-}
-
-var player_classes_dict = {
-	player_classes.KNIGHT : [knight, preload("res://src/characters/base_player/knight/knight_stats.gd")],
-	player_classes.ARCHER : [archer, preload("res://src/characters/base_player/archer/archer_stats.gd")],
-}
-
 func _on_KnightButton_pressed():
-	player_class = player_classes.KNIGHT
+	player_class = knight
 	icon.texture = load("res://assets/textures/knight_v04_new1.png")
 
 func _on_ArcherButton_pressed():
-	player_class = player_classes.ARCHER
+	player_class = archer
 	icon.texture = load("res://assets/textures/Archer.png")
 
 func _on_start_button_pressed():
-	var player = player_classes_dict[player_class][0].instance()
+	var player = player_class.scene.instance()
 	player.name = "player"
 	get_tree().get_root().get_node("Game").load_level(player)#im not sure we can remove get_node from here
--- production/bizzare_v2/src/Menus/character_choise/stats.gd
+++ production/bizzare_v2/src/Menus/character_choise/stats.gd
@@ -9,7 +9,7 @@ var damage = 0
 onready var character_choice = get_parent()
 
 func _process(delta):
-	var player_stats = character_choice.player_classes_dict[character_choice.player_class][1].new() 
+	var player_stats = character_choice.player_class.stats_script.new() 
 	player_stats._ready()  #RABOTAET CHEREZ 04ko
 	set_text(("%-15s" % "\tclass" + "\n").c_unescape())#knight is tmp
 	add_text(("%-15s" % "\thealth" + "%5d" % player_stats.max_health + "\n").c_unescape()) ##FONT PROBLEMS
