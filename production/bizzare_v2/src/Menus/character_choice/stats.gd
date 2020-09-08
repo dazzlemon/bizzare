f49272a5d02cdb9262f0161f3c59bd1f8c743863
DefinitelyLD <drobnyyalex@gmail.com>
2020-09-08 11:17:08 +0300

    font

--- production/bizzare_v2/src/Menus/character_choice/stats.gd
+++ production/bizzare_v2/src/Menus/character_choice/stats.gd
@@ -13,16 +13,16 @@ func _process(delta):
 	player_stats._ready()  #RABOTAET CHEREZ 04ko
 	
 	var player_scene = character_choice.player_class.scene.instance()
-	set_text(("%-15s" % "\tclass" + "%5s" % Utility.get_typeof(player_scene) + "\n").c_unescape())
+	set_text(("%-15s" % "\tclass:" + "%5s" % Utility.get_typeof(player_scene) + "\n").c_unescape())
 	player_scene.free()
 	
-	add_text(("%-15s" % "\thealth" + "%5d" % player_stats.max_health + "\n").c_unescape()) ##FONT PROBLEMS
-	add_text(("%-15s" % "\tarmor" + "%5d" % player_stats.max_armor + "\n").c_unescape())
+	add_text(("%-15s" % "\thealth:" + "%5d" % player_stats.max_health + "\n").c_unescape()) ##FONT PROBLEMS
+	add_text(("%-15s" % "\tarmor:" + "%4s" % player_stats.max_armor + "\n").c_unescape())
 	
 	var player_script = character_choice.player_class.scene_script.new()
 	player_script._ready()
-	add_text(("%-15s" % "\tspeed" + "%5d" % player_script.MAX_SPEED + "\n").c_unescape())
+	add_text(("%-15s" % "\tspeed:" + "%5d" % player_script.MAX_SPEED + "\n").c_unescape())
 	player_script.free()
 	
-	add_text(("%-15s" % "\tdamage" + "%5s" % player_stats.damage + "\n").c_unescape())
+	add_text(("%-15s" % "\tdamage:" + "%4s" % player_stats.damage + "\n").c_unescape())
 	player_stats.free()
