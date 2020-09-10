0e40e2376eaf398f388807206bb5eb5ec176a67a
DefinitelyLD <drobnyyalex@gmail.com>
2020-09-10 23:07:41 +0300

    small fix

--- production/bizzare_v2/src/characters/base_npc/range/harpy/harpy.gd
+++ production/bizzare_v2/src/characters/base_npc/range/harpy/harpy.gd
@@ -2,11 +2,13 @@ class_name Harpy
 extends BaseNPCRange
 
 onready var animation_tree = $AnimationTree
-#var KekW 
+
+
+func _ready():
+	MAX_SPEED = 50
 
 
 func _physics_process(delta):
-	#print(control.get_input_vector())
 	if control.get_input_vector() != Vector2.ZERO:
 		animation_tree["parameters/harpy_idles/blend_position"] = control.get_input_vector()
 	._physics_process(delta)
