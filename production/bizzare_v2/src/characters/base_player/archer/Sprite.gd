363da9358d988fc38434f9aded11775f9d68d25c
DefinitelyLD <drobnyyalex@gmail.com>
2020-09-21 14:41:04 +0300

    items animation

--- production/bizzare_v2/src/characters/base_player/archer/Sprite.gd
+++ production/bizzare_v2/src/characters/base_player/archer/Sprite.gd
@@ -1,12 +1,5 @@
 extends Sprite
 
-
-
 func _process(delta):
 	rotation_degrees += get_angle_to(get_owner().crosshair.global_position)
-	#z_index = int(fposmod(rotation_degrees, 360)/180) + 1
-	var rotation_posmod = fposmod(rotation_degrees, 360)
-	if rotation_posmod < 180:
-		z_index = 2
-	else:
-		z_index = 1
+	z_index = 2 - int(fposmod(rotation_degrees, 360)/180)
--- production/bizzare_v2/src/loot/consumable.gd
+++ production/bizzare_v2/src/loot/consumable.gd
@@ -1,6 +1,14 @@
 class_name Consumable
 extends KinematicBody2D
 
+onready var tween = $Tween
+onready var sprite = $area/sprite
+
+func _ready():
+	tween.interpolate_property(sprite, "position:y", 0, -3, 1 ,Tween.TRANS_SINE, Tween.EASE_IN_OUT)
+	tween.interpolate_property(sprite, "position:y", -3, 0, 1 ,Tween.TRANS_SINE, Tween.EASE_IN_OUT, 1)
+	tween.start()
+
 
 func _on_area_entered(area):
 	die(area)
