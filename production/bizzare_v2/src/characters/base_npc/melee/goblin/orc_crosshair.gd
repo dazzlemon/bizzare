3cbeed353a67c43908a6a2af31f2f0386b03c739
dazzlemon <daanill13470@gmail.com>
2020-10-07 19:31:11 +0300

    razrab godot == govnoed = true

--- production/bizzare_v2/src/characters/base_npc/melee/goblin/orc_crosshair.gd
+++ production/bizzare_v2/src/characters/base_npc/melee/goblin/orc_crosshair.gd
@@ -6,12 +6,6 @@ onready var control = get_node("../control")
 
 func _process(delta):
 	if owner.velocity != Vector2.ZERO:
-		global_position = owner.global_position + owner.velocity.normalized()
+		global_position = owner.global_position + owner.velocity.normalized()# * 100
 	elif control.player_detection_zone.can_see_player():
 		global_position = control.player_detection_zone.player.global_position
-	else:
-		pass
-	._process(delta)
-
-#func _physics_process(delta):
-#	pass
--- production/bizzare_v2/src/characters/hands.gd
+++ production/bizzare_v2/src/characters/hands.gd
@@ -4,10 +4,8 @@ extends Sprite
 var rotation_limit = 29.22
 
 onready var tween = get_node("../../Tween")
-#onready var control = get_node("../../control")
-
 
 func _process(delta):
 	if not tween.is_active():
-		rotation += get_angle_to(owner.crosshair.global_position) 
+		rotation = owner.get_angle_to(owner.crosshair.global_position)
 		z_index = 1 + int(fposmod(rotation_degrees, 360) < 180  + rotation_limit or fposmod(rotation_degrees, 360) > 360 - rotation_limit)
