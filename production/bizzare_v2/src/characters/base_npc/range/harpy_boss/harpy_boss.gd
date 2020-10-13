1633bdec7988dfaa63bc332a5b6966ed8e14092a
DefinitelyLD <drobnyyalex@gmail.com>
2020-10-13 19:19:23 +0300

    harpy first prototype

--- production/bizzare_v2/src/characters/base_npc/range/harpy_boss/harpy_boss.gd
+++ production/bizzare_v2/src/characters/base_npc/range/harpy_boss/harpy_boss.gd
@@ -1,2 +1,9 @@
 class_name HarpyBoss
 extends BaseNPCRange
+
+
+func _physics_process(delta):
+	if velocity != Vector2.ZERO:
+		crosshair.global_position = global_position + velocity.normalized() #velocity.normalized() 
+	animation_tree["parameters/harpy_boss_idles/blend_position"] = (crosshair.global_position - global_position).normalized()
+	._physics_process(delta)
