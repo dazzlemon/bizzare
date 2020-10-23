db047531807aae85ea43f918b79745297fa7aa5c
dazzlemon <daanill13470@gmail.com>
2020-10-23 17:45:39 +0300

    issue10

--- production/bizzare_v2/src/AI/attack_range.gd
+++ production/bizzare_v2/src/AI/attack_range.gd
@@ -3,6 +3,7 @@ extends Area2D
 
 var player = null
 
+onready var animation_player = get_node("../../AttackPlayer")
 onready var parent = get_parent()
 
 func _on_area_entered(area: Area2D) -> void:
@@ -14,3 +15,4 @@ func _on_area_entered(area: Area2D) -> void:
 func _on_area_exited(_area: Area2D) -> void:
 	parent.state = Utility.pick_random([parent.States.IDLE, parent.States.WANDER])
 	player = null
+	animation_player.stop()
