e80513a2feb7f1ec7bdbbe7b5726126b4c29cdea
DefinitelyLD <drobnyyalex@gmail.com>
2020-10-30 16:40:48 +0200

    knight attack animation v1

--- production/bizzare_v2/src/characters/base_player/knight/attack/knight_hitscan.gd
+++ production/bizzare_v2/src/characters/base_player/knight/attack/knight_hitscan.gd
@@ -1,4 +1,22 @@
 class_name KnightHitscan
 extends BaseHitscanAlly
 
+var is_attacked = true
+onready var tween = get_node("../Tween")
+onready var hands = get_node("../pivot/hands")
+
+func _ready():
+	_INTERVAL = 0.5
+
+func _start() -> void:
+	tween.interpolate_property(hands, "rotation_degrees", hands.rotation_degrees , hands.rotation_degrees + 90 , 0.15 , Tween.TRANS_SINE, Tween.EASE_IN , 0)
+	tween.start()
+	is_attacked = not is_attacked
+	._start()
+
+
+func _on_Tween_tween_completed(object, key):
+	if is_attacked:
+		is_attacked = not is_attacked
+		tween.interpolate_property(hands, "rotation_degrees", hands.rotation_degrees , hands.rotation_degrees - 90 , 0.35 , Tween.TRANS_SINE, Tween.EASE_IN , 0)
 
--- production/bizzare_v2/src/characters/base_player/knight/knight_stats.gd
+++ production/bizzare_v2/src/characters/base_player/knight/knight_stats.gd
@@ -4,6 +4,6 @@ extends BasePlayerStats
 
 func _ready():
 	self.damage = 20####################RAZRAB TUPOY DEGENERAT, POCHEMU setget ne rabotaet?????????????
-	max_health = 100
+	max_health = 1000
 	max_armor = 10
 	._ready()
