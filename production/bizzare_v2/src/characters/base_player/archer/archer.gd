d72dafaf26e68ec27eb9f46545aee5b85cf32387
DefinitelyLD <drobnyyalex@gmail.com>
2020-11-05 15:55:51 +0200

    small fixes

--- production/bizzare_v2/src/characters/base_player/archer/archer.gd
+++ production/bizzare_v2/src/characters/base_player/archer/archer.gd
@@ -5,6 +5,7 @@ extends BasePlayer
 onready var attack = get_node("attack")
 onready var timer = get_node("AttackPlayer/Timer")
 onready var attack_player = $AttackPlayer
+#onready var spell_1 = get_node("spell_1")
 
 
 func _ready():
@@ -14,6 +15,6 @@ func _ready():
 
 func _physics_process(delta):
 	._physics_process(delta)
-	if Input.is_action_pressed("attack") or Input.is_action_pressed("spell_1") : 
+	if Input.is_action_pressed("attack") or Input.is_action_pressed("spell_1") : #NEED TO FIX "Q" Trigering attack animation , when spell isn't ready
 		attack_player.play("attack")
 
