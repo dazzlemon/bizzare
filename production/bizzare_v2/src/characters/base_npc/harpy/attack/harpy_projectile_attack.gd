40db5d63d39dc6d127c29c65190d3098bdd07190
dazzlemon <daanill13470@gmail.com>
2020-11-11 19:12:29 +0200

    harpy rebased

--- production/bizzare_v2/src/characters/base_npc/harpy/attack/harpy_projectile_attack.gd
+++ production/bizzare_v2/src/characters/base_npc/harpy/attack/harpy_projectile_attack.gd
@@ -2,7 +2,7 @@ class_name HarpyProjectileAttack
 extends BaseProjectileAttackEnemy
 
 func _ready():
-	projectile = preload("res://src/characters/base_npc/range/harpy/attack/harpy_projectile.tscn")
+	projectile = preload("res://src/characters/base_npc/harpy/attack/harpy_projectile.tscn")
 	_INTERVAL = 2
 
 
