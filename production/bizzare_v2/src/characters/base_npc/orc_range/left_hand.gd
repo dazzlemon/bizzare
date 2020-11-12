e67a20fc00001a703a9d8e5c91c61c02a5774b03
dazzlemon <daanill13470@gmail.com>
2020-11-12 13:12:54 +0200

    range orc fixed

--- production/bizzare_v2/src/characters/base_npc/orc_range/left_hand.gd
+++ production/bizzare_v2/src/characters/base_npc/orc_range/left_hand.gd
@@ -2,7 +2,6 @@ class_name LeftHand
 extends Sprite
 
 onready var main_hand = get_node("../hands")
-var max_val_z = 4
 
 func _process(delta):
-	z_index = Utility.map(main_hand.z_index, 1, max_val_z, 1, 3)
+	z_index = main_hand.z_index
--- production/bizzare_v2/src/characters/base_npc/orc_range/orc_range_hands.gd
+++ production/bizzare_v2/src/characters/base_npc/orc_range/orc_range_hands.gd
@@ -1,7 +1,2 @@
 class_name OrcRangeHands
 extends BaseHands
-
-func _update_z(rot_deg):
-	._update_z(rot_deg)
-	z_index = (Utility.map(z_index, 1, 2, 1, 4))
-
