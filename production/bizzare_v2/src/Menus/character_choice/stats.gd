d4f21417a8482cec2ffa7efaa58650101c69dc4a
dazzlemon <daanill13470@gmail.com>
2020-11-18 00:20:26 +0200

    char choice refactor + placeholder monospaced font

--- production/bizzare_v2/src/Menus/character_choice/stats.gd
+++ production/bizzare_v2/src/Menus/character_choice/stats.gd
@@ -1,28 +1,47 @@
 extends RichTextLabel
 
 var class_type = "knight"
-var health = 0
-var armor = 0
-var speed = 0
-var damage = 0
 
 onready var character_choice = get_parent()
 
 func _process(delta):
+	print_stats(get_stats())
+
+
+func print_stats(stats):
+	set_text("")
+	for k in stats:
+		add_text(_str(k, stats[k]))
+
+
+func get_stats():
 	var player_stats = character_choice.player_class.stats_script.new()
-	player_stats._ready()  #RABOTAET CHEREZ 04ko
-	
+	player_stats._ready()
+	var stats = {
+		"class" : _get_typeof_player(),
+		"health" : player_stats.max_health,
+		"armor" : player_stats.max_armor,
+		"speed" : _get_speed(),
+		"damage" : player_stats.damage,
+		}
+	player_stats.free()
+	return stats
+
+
+func _get_typeof_player():
 	var player_scene = character_choice.player_class.scene.instance()
-	set_text(("%-15s" % "\tclass:" + "%5s" % Utility.get_typeof(player_scene) + "\n").c_unescape())
+	var ret = Utility.get_typeof(player_scene)
 	player_scene.free()
-	
-	add_text(("%-15s" % "\thealth:" + "%5d" % player_stats.max_health + "\n").c_unescape()) ##FONT PROBLEMS
-	add_text(("%-15s" % "\tarmor:" + "%4s" % player_stats.max_armor + "\n").c_unescape())
-	
+	return ret
+
+
+func _get_speed():
 	var player_script = character_choice.player_class.scene_script.new()
 	player_script._ready()
-	add_text(("%-15s" % "\tspeed:" + "%5d" % player_script.MAX_SPEED + "\n").c_unescape())
+	var ret = player_script.MAX_SPEED
 	player_script.free()
-	
-	add_text(("%-15s" % "\tdamage:" + "%4s" % player_stats.damage + "\n").c_unescape())
-	player_stats.free()
+	return ret
+
+
+func _str(_name, val):
+	return ("%-10s" % ("\t" + _name) + "%6s" % val + "\n").c_unescape()
