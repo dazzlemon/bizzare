40f12c9298b439edcab381714843c449c84c1379
dazzlemon <daanill13470@gmail.com>
2020-11-19 00:11:24 +0200

    npc health bar colors optimized

--- production/bizzare_v2/src/etc/status_bars/npc_health/nps_health_bar.gd
+++ production/bizzare_v2/src/etc/status_bars/npc_health/nps_health_bar.gd
@@ -1,16 +1,12 @@
 class_name NPCHealthBar
 extends StatusBar
 
-#var full_hp_color = Color( 0.2, 0.8, 0.2, 1 ) #limegreen
-var almost_full_hp_color = Color( 0.68, 1, 0.18, 1 ) #greenyellow
-var half_hp_color = Color("#e1f120")  # gold
-var low_hp_color = Color("#fbbb12") #Orangered
-var very_low_hp_color = Color ("#cc2e2e") #red
-
-export (float , 0, 1, 0.05) var almost_full_hp = 0.9
-export (float , 0, 1, 0.05) var half_hp = 0.5
-export (float , 0, 1, 0.05) var low_hp = 0.25
-export (float , 0, 1, 0.05) var very_low_hp = 0.1
+var color_map = {
+	Color(Color.indianred) : 0.1,
+	Color(Color.orange) : 0.25,
+	Color(Color.gold) : 0.5,
+	Color(Color.yellowgreen) : 0.9,
+}
 
 func _process(delta):
 	assign_color()
@@ -18,20 +14,12 @@ func _process(delta):
 	
 
 func _ready():
-	max_value_str = "max_health" 
-	value_str = "health" 
-	#status_bar.tint_progress = full_hp_color
+	max_value_str = "max_health"
+	value_str = "health"
 	._ready()
 
 func assign_color():
-	if stats.health < status_bar.max_value * very_low_hp:
-		status_bar.tint_progress = very_low_hp_color
-	elif stats.health < status_bar.max_value * low_hp:
-		status_bar.tint_progress = low_hp_color
-	elif stats.health < status_bar.max_value * half_hp:
-		status_bar.tint_progress = half_hp_color
-	elif stats.health < status_bar.max_value * almost_full_hp:
-		status_bar.tint_progress = almost_full_hp_color
-
-	
-
+	for color in color_map:
+		if stats.health < status_bar.max_value * color_map[color]:
+			status_bar.tint_progress = color
+			break
--- production/bizzare_v2/src/etc/status_bars/status_bar.gd
+++ production/bizzare_v2/src/etc/status_bars/status_bar.gd
@@ -6,10 +6,6 @@ onready var stats = get_node("../../stats")
 onready var update_tween = $UpdateTween
 onready var animation = $animation
 onready var label = $RichTextLabel
-#onready var pulse_tween = $PulseTween
-
-#export (Color) var pulse_color = Color ("#902a34")
-#var is_pulsing = false
 var max_value_str = "max_health" #DEFAULT VALUE
 var value_str = "health" #DEFAULT VALUE
 
@@ -36,8 +32,3 @@ func update_value():
 		status_bar.value = stats.get(value_str)
 		update_tween.interpolate_property(animation, "value", animation.value, stats.get(value_str), 0.175, Tween.TRANS_SINE, Tween.EASE_IN_OUT, 0.175)
 		update_tween.start()
-#		if is_pulsing:
-#			pulse()
-
-#func pulse():
-#	pass
