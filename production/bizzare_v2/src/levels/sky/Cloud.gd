3a0594e581a83e752e63ebab5ce5518400b43902
dazzlemon <daanill13470@gmail.com>
2020-11-19 18:57:42 +0200

    sky almost done

--- production/bizzare_v2/src/levels/sky/Cloud.gd
+++ production/bizzare_v2/src/levels/sky/Cloud.gd
@@ -3,17 +3,21 @@ extends Node2D
 onready var sprite = $Sprite
 var velocity
 var direction
-var max_range 
+var max_range
+
+var offset = Vector2.ZERO
 
 func _ready():
-	sprite.rotation = 360/4 * (randi()%4)
+	velocity = rand_range(300, 600)#(3, 6)
+	
+	sprite.rotation_degrees = 360 / 4 * (randi() % 4)
 	sprite.scale *= rand_range(1, 2)
-	velocity = rand_range(30, 60)#3,6
 	sprite.modulate.a = rand_range(0.28, 0.36)
-	sprite.frame = randi()%4
+	sprite.frame = randi() % 4
 
 
 func _process(delta):
+	offset += velocity * delta * direction#i had to make my own offset because godot's devs didnt
 	position += velocity * delta * direction
-	if position.length() >= max_range:
+	if offset.length() >= max_range:
 		queue_free()
--- production/bizzare_v2/src/levels/sky/sky.gd
+++ production/bizzare_v2/src/levels/sky/sky.gd
@@ -1,20 +1,19 @@
 extends Node2D
 
+var cloud = preload("res://src/levels/sky/Cloud.tscn")
+var size = 1000
 
 onready var timer = $Spawn_timer
 onready var spawner = $Spawner
-var cloud = preload("res://src/levels/sky/Cloud.tscn")
-var size = 200
-
 
 func _ready():
-	#randomize()
-	rotation = 360/8 * randi()%8
+	randomize()
+	rotation_degrees = 360 / 8 * (randi() % 8)
 	_on_timeout()
 
 
 func _on_timeout():
-	timer.start(rand_range(2, 5)) #15 20
+	timer.start(rand_range(0.3, 0.5))#(15, 20))
 	spawn_clouds()
 
 
@@ -23,12 +22,12 @@ func spawn_clouds():
 	while _position < size:
 		instance_cloud(_position)
 		_position += rand_range(100 ,200)
-		
 
 
 func instance_cloud(_position):
 	var cloud_instance = cloud.instance()
-	cloud_instance.global_position = global_position + Vector2(0, _position).rotated(rotation)
+	cloud_instance.position = Vector2.ZERO
+	cloud_instance.global_position = global_position + Vector2(-size / 2, _position).rotated(rotation)
 	cloud_instance.direction = Vector2.RIGHT.rotated(rotation)
 	cloud_instance.max_range = size
 	get_parent().call_deferred("add_child", cloud_instance)
