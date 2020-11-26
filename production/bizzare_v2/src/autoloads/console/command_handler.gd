0699088c548e4192dde482fe3234b2b060eadeb9
DefinitelyLD <drobnyyalex@gmail.com>
2020-11-26 22:45:43 +0200

    console v2

--- production/bizzare_v2/src/autoloads/console/command_handler.gd
+++ production/bizzare_v2/src/autoloads/console/command_handler.gd
@@ -9,29 +9,45 @@ enum{
 }
 
 const commands = [
-	["set_health", [FLOAT]],
-	["set_armor", [FLOAT]],
+	["health", [FLOAT]],
+	["armor", [FLOAT]],
+	["max_health", [FLOAT]],
+	["max_armor", [FLOAT]],
+	["damage", [FLOAT]],
+	["clear", []],
 ]
 
-func set_health(health):
-	set_value("health", health)
-#	health = float(health)
-#	var player = get_tree().get_root().get_node("Game/level/Trees/YSort/player")
-#	var stats = player.get_node("stats")
-#	stats.health = health
-#	return str ("Successfully set health to ", health)
+func health(value):
+	return set_value("health", value)	
 
 
-func set_armor(armor):
-	armor = float(armor)
-	var player = get_tree().get_root().get_node("Game/level/Trees/YSort/player")
-	var stats = player.get_node("stats")
-	stats.armor = armor
-	return str ("Successfully set armor to ", armor)
+func max_health(value):
+	return set_value("max_health", value)
+
+
+func armor(value):
+	return set_value("armor", value)
+
+
+func max_armor(value):
+	return set_value("max_armor", value)
+
+
+func damage(value):
+	return set_value("damage", value)
 
 
 func set_value(_name, value):
+	value = float(value)
 	var player = get_tree().get_root().get_node("Game/level/Trees/YSort/player")
 	var stats = player.get_node("stats")
-	stats.set(get(_name), value)
-	return str ("Successfully set" + _name + "to ", value)
+	stats[_name] = value
+	return str ("Successfully set " + _name + " to ", value)
+
+func clear():
+	var output = get_node("../").output
+	output.select_all()
+	output.cut()
+	output.set_line(0, " Debug console")
+	return str("")
+	
