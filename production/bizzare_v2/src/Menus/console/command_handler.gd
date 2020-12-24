c292fc5f065c9afbe35065e8259fc56fdd130a99
DefinitelyLD <drobnyyalex@gmail.com>
2020-12-24 15:27:42 +0200

    impr

--- production/bizzare_v2/src/Menus/console/command_handler.gd
+++ production/bizzare_v2/src/Menus/console/command_handler.gd
@@ -17,6 +17,7 @@ const commands = [
 	["clear", []],
 	["no_clip", []],
 	["spawn",[STRING]],
+	["help",[]]
 ]
 
 var mobs = {
@@ -28,13 +29,21 @@ var mobs = {
 	preload("res://src/characters/base_npc/shadow/shadow.tscn") : "shadow",
 }
 
-
+func help():
+	return("1.health,max_health,damage,etc +value\n-sets property's value\n2.clear - clears console\n3.no_clip - turning on/off collisions\n for player\n4.spawn + mob_name[harpy,slime,\norc_melee,orc_range,shadow]")
 
 func set_value(_name, value):
 	var player = get_tree().get_root().get_node("Game/level/Trees/YSort/player")
 	var stats = player.get_node("stats")
 	value[0] = float(value[0])
-	stats[_name] = value[0]
+	if _name == "health" and value[0] > stats.max_health:
+		stats.max_health = value[0]
+		stats[_name] = value[0]
+	elif _name == "armor" and value[0] > stats.max_armor:
+		stats.max_armor= value[0]
+		stats[_name] = value[0]
+	else:
+		stats[_name] = value[0]
 	return str ("Successfully set " + _name + " to ", value[0])
 
 
@@ -50,11 +59,12 @@ func clear():
 	output.cut()
 	output.set_line(0, " Debug console")
 	return str("")
-	
+
+
 func spawn(url):
 	for mob in mobs:
 		if url == mobs[mob]:
 			var instance = mob.instance()
 			instance.global_position = get_tree().get_root().get_node("Game/level/Trees/YSort/player").global_position + Vector2(20,20)
 			get_tree().get_root().get_node("Game/level/Trees/YSort").call_deferred("add_child", instance)
-	return str("")
+	return (str(url) + " successfully spawned")
--- production/bizzare_v2/src/characters/base_npc/orc_range/attack/orc_range_projectile.gd
+++ production/bizzare_v2/src/characters/base_npc/orc_range/attack/orc_range_projectile.gd
@@ -3,3 +3,5 @@ extends BaseProjectileEnemy
 
 func _ready():
 	rotation += get_angle_to(get_node("../orc_range/crosshair").global_position) 
+	speed = 200
+	self.damage = 1
