798c0c87c1117783eca0c63228ec642597ce7753
DefinitelyLD <drobnyyalex@gmail.com>
2021-01-05 16:16:59 +0200

    colliders fixed

--- production/bizzare_v3/src/Game/game.gd
+++ production/bizzare_v3/src/Game/game.gd
@@ -7,8 +7,8 @@ func _ready():
 func load_level(player):
 	if has_node("level"):
 		remove_child($level) 
-	var new_level = preload("res://src/levels/room.tscn").instance()
-	new_level.get_node("YSort").add_child(player)
+	var new_level = preload("res://src/levels/room2.tscn").instance()
+	new_level.get_node("Trees/YSort").add_child(player)
 	player.z_as_relative = true
 	player.global_position = Vector2(0, 0)
 	add_child(new_level)
--- production/bizzare_v3/src/base_char/base_player/knight/hands.gd
+++ production/bizzare_v3/src/base_char/base_player/knight/hands.gd
@@ -3,5 +3,5 @@ extends BaseHands
 
 
 func set_rotation(rot):
-	rotation = -PI / 4 + rot
+	rotation = 2 * PI  + rot
 
